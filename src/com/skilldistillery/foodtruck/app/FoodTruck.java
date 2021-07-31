e220fc7fa27fa47a27271fb05fa2e9a185dab216
Mathew Smyth <smythnwesson@gmail.com>
2021-07-31 16:00:31 -0600

    test push

--- src/com/skilldistillery/foodtruck/app/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/app/FoodTruck.java
@@ -1,6 +1,67 @@
 package com.skilldistillery.foodtruck.app;
 
 public class FoodTruck {
-
+	private String name;
+	private int rating;
+	private String food;
+	private int UID = 0;
+	private static int nextFoodTruckId = 1;
 	
+	public FoodTruck() {
+		this.UID = nextFoodTruckId;
+		nextFoodTruckId++;
+	}
+
+	public FoodTruck(String name, int rating, String food) {
+		this();
+		this.name = name;
+		this.rating = rating;
+		this.food = food;
+		
+	}
+	
+	//methods
+	
+	
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getRating() {
+		return rating;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+
+	public int getUID() {
+		return UID;
+	}
+
+	public void setUID(int uID) {
+		UID = uID;
+	}
+	public String getFoodTruckData() {
+		String output = "name=" + name + ", rating=" + rating
+				+ ", UID=" + UID;
+		return output;
+	}
+
+	public void displayFoodTruck() {
+		String foodTruckData = this.toString();
+		System.out.println(foodTruckData);
+	}
+	@Override
+	public String toString() {
+		return "FoodTruck [name=" + name + ", rating=" + rating
+				+ ", UID=" + UID + "]";
+	}
+
+
 }
--- src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -1,9 +1,96 @@
 package com.skilldistillery.foodtruck.app;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class FoodTruckApp {
+	private int numFoodTrucks;
+	private int MAX_TRUCKS = 5;
+	private FoodTruck[] foodTrucks = new FoodTruck[MAX_TRUCKS];
+	Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
 
+		FoodTruckApp foodTrucksApp = new FoodTruckApp();
+		foodTrucksApp.run();
+
+	}
+
+	public void run() {
+		boolean keepGoing = true;
+		createFoodTruck();
+		do {
+			System.out.println("Please select an option from the menu below using 1-4 as an input: \n");
+			System.out.println("----------------------------------------");
+			System.out.println("1) List all food trucks");
+			System.out.println("2) See the average rating of food trucks");
+			System.out.println("3) Highest rated food truck");
+			System.out.println("4) QUIT");
+			System.out.println("----------------------------------------");
+
+			int foodTruckMenu = sc.nextInt();
+
+			switch (foodTruckMenu) {
+			case 1:
+				allFoodTrucks();
+				break;
+			case 2:
+				System.out.println();
+				break;
+			case 3:
+				System.out.println();
+				break;
+			case 4:
+				System.out.println("Goodbye");
+				keepGoing = false;
+				break;
+
+			default:
+				System.out.println("Input invalid. Please try again.");
+			}
+
+		} while (keepGoing);
+		sc.close();
+	}
+
+	public double averageRating(double avg) {
+		return 0.0;
+	}
+
+	public void allFoodTrucks() {
+		for (FoodTruck foodTruck : foodTrucks) {
+			if(foodTruck == null) {
+				continue;
+			}
+			System.out.println(foodTruck);
+		}
+	}
+
+	public double highestRated() {
+		return 0.0;
+	}
+
+	public void createFoodTruck() {
+		for (int i = 0; i < foodTrucks.length; i++) {
+
+			System.out.println("Please enter a food truck: ");
+			String name = sc.nextLine();
+
+			if (name.equalsIgnoreCase("quit")) {
+				break;
+			}
+			System.out.println("Please enter the rating you wish to give the food truck from 1-5, \n"
+					+ "1 being the worst 5 being the best: ");
+			int rating = sc.nextInt();
+			System.out.println();
+			
+			sc.nextLine();
+			
+			System.out.println("Please enter the type of food served");
+			String food = sc.nextLine();
+			FoodTruck foodT = new FoodTruck(name, rating, food);
+			foodTrucks [i] = foodT; 
+		}
 	}
 
 }
